540c249288747f87b70358b945d92e5cb2c3ad04
vladdossik <vladdosiik7540@gmail.com>
2021-11-11 13:48:57 +0300

    plus

--- src/main/java/com/nekromant/finance/contants/Title.java
+++ src/main/java/com/nekromant/finance/contants/Title.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 public enum Title {
     MANAGE_CATEGORIES("Управление категориями"),
     KEYWORDS("Ключевые слова"),
-    ADD_CATEGORY(Character.toString('➕')),
+    ADD_CATEGORY(Character.toString(0x00002795)),
     EDIT_NAME("Изменить название"),
     DELETE_CATEGORY("Удалить категорию"),
     EDIT_KEYWORDS("Редактировать ключевые слова"),
