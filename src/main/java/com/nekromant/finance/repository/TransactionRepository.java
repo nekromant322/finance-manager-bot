236b95a3e245d3481d8f16100f6429165a111b52
vladdosiik <vladdosiik7540@gmail.com>
2022-06-19 21:38:40 +0300

    added swagger and transactionController to get all user transactions

--- src/main/java/com/nekromant/finance/repository/TransactionRepository.java
+++ src/main/java/com/nekromant/finance/repository/TransactionRepository.java
@@ -3,5 +3,9 @@ package com.nekromant.finance.repository;
 import com.nekromant.finance.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findAllByClientId(Long clientId);
 }
